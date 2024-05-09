39860929242a1aac61e3b174eaa69ff1659b8ca6
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-05-09 11:43:26 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavBar.css';
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <NavLink to="/" activeClassName="active" exact>Home</NavLink>
-      <NavLink to="/directors" activeClassName="active">Directors</NavLink>
-      <NavLink to="/actors" activeClassName="active">Actors</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </nav>
   );
 }
