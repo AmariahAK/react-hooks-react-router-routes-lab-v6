9243dfbd41f9ce74c7f6f881f8133f2b7a9d9846
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-05-09 11:37:52 +0300

    full test

--- src/routes.js
+++ src/routes.js
@@ -1,50 +1,31 @@
-// In your Routes.js file
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Directors from './pages/Directors';
-import Actors from './pages/Actors';
-import Movie from './pages/Movie';
-import ErrorPage from './pages/ErrorPage'; 
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   {
-    path: '/',
-    component: Home,
-    exact: true
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: '/directors',
-    component: Directors,
-    exact: true
+    path: "/actors",
+    element: <Actors />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: '/actors',
-    component: Actors,
-    exact: true
+    path: "/directors",
+    element: <Directors />,
+    errorElement: <ErrorPage />,
   },
+
   {
-    path: '/movie/:id',
-    component: Movie,
-    exact: true
+    path: "/movie/:id",
+    element: <Movie />,
+    errorElement: <ErrorPage />,
   },
-  {
-    path: '*',
-    component: ErrorPage
-  }
 ];
 
-const Routes = () => (
-  <Switch>
-    {routes.map((route, index) => (
-      <Route
-        key={index}
-        path={route.path}
-        exact={route.exact}
-        component={route.component}
-      />
-    ))}
-  </Switch>
-);
-
-export default Routes;
+export default routes;
