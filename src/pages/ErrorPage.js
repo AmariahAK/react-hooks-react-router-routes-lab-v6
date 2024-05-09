3761ecd647c646d9b2b35a0351b8c83cbf081e2d
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2024-05-09 11:40:45 +0300

    full test

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import NavBar from "../components/NavBar";
 
 function ErrorPage() {
   return (
     <>
-      <NavBar />
-      <h1>Oops! Looks like something went wrong.</h1>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
     </>
   );
 }
